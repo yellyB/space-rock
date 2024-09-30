9b39ed3fd3b05a493dcf0894b0d83d799963e195
yelly <kk3p1q1@gmail.com>
2024-10-01 05:38:04 +0900

    imp: 바위 사격시 애니메이션 추가

--- rock.gd
+++ rock.gd
@@ -4,6 +4,7 @@ var screensize = Vector2.ZERO
 var size
 var radius
 var scale_factor = 0.2  # size를 곱해 Sprite2D 스케일, 콜리전 반지름 등 설정
+signal exploded
 
 func start(_position, _velocity, _size):
 	position = _position
@@ -13,9 +14,10 @@ func start(_position, _velocity, _size):
 	radius = int($Sprite2D.texture.get_size().x / 2 * $Sprite2D.scale.x)
 	var shape = CircleShape2D.new()
 	shape.radius = radius
-	$CollisionShape2D.shape = shape  # 바위 size에 따라 콜리전 크기 계산 
+	$CollisionShape2D.shape = shape  # 바위 size에 따라 콜리전 크기 계산
 	linear_velocity = _velocity
-	angular_velocity = randf_range(-PI, PI)  # 시계방향 혹은 반시계방향으로 설정 
+	angular_velocity = randf_range(-PI, PI)  # 시계방향 혹은 반시계방향으로 설정
+	$Explosion.scale = Vector2.ONE * 0.75 * size  # 폭발 크기를 바위 크기에 맞춰 조정
 
 
 func _integrate_forces(physics_state):
@@ -24,3 +26,15 @@ func _integrate_forces(physics_state):
 	xform.origin.x = wrapf(xform.origin.x, 0 - radius, screensize.x + radius)
 	xform.origin.y = wrapf(xform.origin.y, 0 - radius, screensize.y + radius)
 	physics_state.transform = xform
+
+func explode():
+	$CollisionShape2D.set_deferred("disabled", true)  # CollisionShape2D 노드의 충돌 감지를 비활성화
+	$Sprite2D.hide()
+	$Explosion/AnimationPlayer.play("explosion")
+	$Explosion.show()  # Explosion 노드를 화면에 표시
+	exploded.emit(size, radius, position, linear_velocity)  # exploded라는 시그널을 방출
+	# 폭발된 위치에서 멈출 수 있도록 velocity 값 초기화
+	linear_velocity = Vector2.ZERO
+	angular_velocity = 0
+	await $Explosion/AnimationPlayer.animation_finished  # 애니메이션 종료까지 기다린 후 다음 실행
+	queue_free()  # 노드를 씬에서 제거	
