20a854dc0045e22bae81baf5862411bbf7424046
yelly <kk3p1q1@gmail.com>
2024-10-19 16:12:03 +0900

    imp: game interface script

---
+++ hud.gd
@@ -0,0 +1,37 @@
+extends CanvasLayer
+
+signal start_game
+
+@onready var lives_counter = $MarginContainer/HBoxContainer/LivesCounter.get_children()
+@onready var score_label = $MarginContainer/HBoxContainer/ScoreLabel
+@onready var message = $VBoxContainer/Message
+@onready var start_button = $VBoxContainer/Message/StartButton
+
+
+func show_message(text):
+	message.text = text
+	message.show()
+	$Timer.start()
+	
+func update_score(value):
+	score_label.text = str(value)
+	
+func update_lives(value):
+	for life in 3:
+		lives_counter[life].visible = value > life
+
+func game_over():
+	show_message("Game Over")
+	await $Timer.timeout
+	start_button.show()
+
+
+func _on_start_button_pressed():
+	start_button.hide()
+	start_game.emit()
+
+
+func _on_timer_timeout():
+	message.hide()
+	message.text = ''
+	
